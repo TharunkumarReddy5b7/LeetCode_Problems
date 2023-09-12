f9cbcb14bad20c841ac3c9611aab412e84dea54a
pandu k <ktr.tharunkumarreddy@gmail.com>
2023-09-12 12:04:51 +0530

    Leetcode P17 Commited

---
+++ P17_LetterCombinationsPhoneNumber.py
@@ -0,0 +1,19 @@
+#17 LetterCombinationsOfPhoneNumber
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        n = len(digits)
+        if n == 0:
+            return []
+        dic={'2':['a','b','c'], '3':['d','e','f'], '4':['g','h','i'], '5':['j','k','l'], '6':['m','n','o'], '7':['p','q','r','s'], '8':['t','u','v'], '9':['w','x','y','z']}
+        res=[]
+        
+        def fun(r):
+            if len(r)==n:
+                res.append(r)
+            else:
+                for i in dic[digits[len(r)]]:
+                    fun(r+i)
+        
+        fun("")
+        return res
+        
